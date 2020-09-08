d1293f1c7d90d7c4c92783ed75f7bdfea28447c0
mykolaskiau <69675467+mykolaskiau@users.noreply.github.com>
2020-09-08 15:53:58 +0300

    Add files via upload

--- bandom_isyoutube.py
+++ bandom_isyoutube.py
@@ -5,7 +5,12 @@ min = (youtube.length) // 60
 sec = (youtube.length) - min*60
 print(sec)
 print("Title:", ((youtube).title), "\nLenght:",min,":",sec)
-print("Downloading the video")
-video = youtube.streams.first()
-video.download()
-print("Done")
+print("Are you sure you want to download this video [Y/N]?")
+anw = input(":")
+if anw == ("Y"):
+    print("Downloading the video")
+    video = youtube.streams.first()
+    video.download()
+    print("Done")
+else:
+    print("Not downloading")     
